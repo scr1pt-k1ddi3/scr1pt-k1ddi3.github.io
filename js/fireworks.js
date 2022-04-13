b2b57b9aca9a32883f6c663b8c0e3ff2c047a04f
scr1pt_k1ddi3 <1029646295@qq.com>
2022-04-13 13:29:11 +0800

    Site updated: 2022-04-13 13:29:11

--- js/fireworks.js
+++ js/fireworks.js
@@ -1,148 +1,135 @@
-//canvas鼠标点击烟花特效
- 
-let endpos = (x, y) => {
-    let angle = random(0, 360) * Math.PI / 180,
-      value = random(20, 150),
-      radius = [-1, 1][random(0, 1)] * value;
+function clickEffect() {
+    let balls = [];
+    let longPressed = false;
+    let longPress;
+    let multiplier = 0;
+    let width, height;
+    let origin;
+    let normal;
+    let ctx;
+    const colours = ["#F73859", "#14FFEC", "#00E0FF", "#FF99FE", "#FAF15D"];
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    canvas.setAttribute("style", "width: 100%; height: 100%; top: 0; left: 0; z-index: 99999; position: fixed; pointer-events: none;");
+    const pointer = document.createElement("span");
+    pointer.classList.add("pointer");
+    document.body.appendChild(pointer);
    
-    return {
-      x: x + radius * Math.cos(angle),
-      y: y + radius * Math.sin(angle)
+    if (canvas.getContext && window.addEventListener) {
+      ctx = canvas.getContext("2d");
+      updateSize();
+      window.addEventListener('resize', updateSize, false);
+      loop();
+      window.addEventListener("mousedown", function(e) {
+        pushBalls(randBetween(10, 20), e.clientX, e.clientY);
+        document.body.classList.add("is-pressed");
+        longPress = setTimeout(function(){
+          document.body.classList.add("is-longpress");
+          longPressed = true;
+        }, 500);
+      }, false);
+      window.addEventListener("mouseup", function(e) {
+        clearInterval(longPress);
+        if (longPressed == true) {
+          document.body.classList.remove("is-longpress");
+          pushBalls(randBetween(50 + Math.ceil(multiplier), 100 + Math.ceil(multiplier)), e.clientX, e.clientY);
+          longPressed = false;
+        }
+        document.body.classList.remove("is-pressed");
+      }, false);
+      window.addEventListener("mousemove", function(e) {
+        let x = e.clientX;
+        let y = e.clientY;
+        pointer.style.top = y + "px";
+        pointer.style.left = x + "px";
+      }, false);
+    } else {
+      console.log("canvas or addEventListener is unsupported!");
     }
-  }
    
-  let random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
    
-  class Circle {
-    constructor(x, y) {
-      this.r = random(9, 13)
-      this.opos = {}
-      this.x = this.opos.x = x
-      this.y = this.opos.y = y
-   
-      this.colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C']
-      this.color = this.colors[random(0, this.colors.length)];
-      this.tpos = endpos(x, y)
+    function updateSize() {
+      canvas.width = window.innerWidth * 2;
+      canvas.height = window.innerHeight * 2;
+      canvas.style.width = window.innerWidth + 'px';
+      canvas.style.height = window.innerHeight + 'px';
+      ctx.scale(2, 2);
+      width = (canvas.width = window.innerWidth);
+      height = (canvas.height = window.innerHeight);
+      origin = {
+        x: width / 2,
+        y: height / 2
+      };
+      normal = {
+        x: width / 2,
+        y: height / 2
+      };
+    }
+    class Ball {
+      constructor(x = origin.x, y = origin.y) {
+        this.x = x;
+        this.y = y;
+        this.angle = Math.PI * 2 * Math.random();
+        if (longPressed == true) {
+          this.multiplier = randBetween(14 + multiplier, 15 + multiplier);
+        } else {
+          this.multiplier = randBetween(6, 12);
+        }
+        this.vx = (this.multiplier + Math.random() * 0.5) * Math.cos(this.angle);
+        this.vy = (this.multiplier + Math.random() * 0.5) * Math.sin(this.angle);
+        this.r = randBetween(8, 12) + 3 * Math.random();
+        this.color = colours[Math.floor(Math.random() * colours.length)];
+      }
+      update() {
+        this.x += this.vx - normal.x;
+        this.y += this.vy - normal.y;
+        normal.x = -2 / window.innerWidth * Math.sin(this.angle);
+        normal.y = -2 / window.innerHeight * Math.cos(this.angle);
+        this.r -= 0.3;
+        this.vx *= 0.9;
+        this.vy *= 0.9;
+      }
     }
    
-    creatCircle(ctx) {
-      ctx.beginPath()
-      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
-      ctx.closePath()
-      ctx.fillStyle = this.color
-      ctx.fill()
+    function pushBalls(count = 1, x = origin.x, y = origin.y) {
+      for (let i = 0; i < count; i++) {
+        balls.push(new Ball(x, y));
+      }
     }
    
-    //根据不同距离段设置前行的步伐，分为3个阶段，离出发点近的那一段为速度最快，中间为速度一般，最远为速度最慢
-    //区分目标点小于出发点的情况
-    //ratio为两点之间的距离的行走比例，比例数值越大，行走越慢
-    moveFun(start, end, current) {
-        let s = random(26, 35),
-          m = random(20, 25),
-          f = random(15, 20),
-          ff = start.x + ~~((end.x - start.x) / 3),
-          mm = ff + ~~((end.x - start.x) / 3),
-          ratio = end.x >= start.x ? (Math.max(current, ff) == current ? (Math.max(current, mm) == current ? s : m) : f) : (Math.max(current, ff) == current ? f : (Math.max(current, mm) == current ? m : s)),
-          mp = {
-            x: end.x - start.x,
-            y: end.y - start.y
-          };
+    function randBetween(min, max) {
+      return Math.floor(Math.random() * max) + min;
+    }
    
-        return {
-          x: Math.abs(mp.x / ratio),
-          y: Math.abs(mp.y / ratio)
+    function loop() {
+      ctx.fillStyle = "rgba(255, 255, 255, 0)";
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for (let i = 0; i < balls.length; i++) {
+        let b = balls[i];
+        if (b.r < 0) continue;
+        ctx.fillStyle = b.color;
+        ctx.beginPath();
+        ctx.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
+        ctx.fill();
+        b.update();
+      }
+      if (longPressed == true) {
+        multiplier += 0.2;
+      } else if (!longPressed && multiplier >= 0) {
+        multiplier -= 0.4;
+      }
+      removeBall();
+      requestAnimationFrame(loop);
+    }
+   
+    function removeBall() {
+      for (let i = 0; i < balls.length; i++) {
+        let b = balls[i];
+        if (b.x + b.r < 0 || b.x - b.r > width || b.y + b.r < 0 || b.y - b.r > height || b.r < 0) {
+          balls.splice(i, 1);
         }
       }
-      
-      //根据计算出来的移动值去移动
-      //如果目标坐标大于原始坐标则向右移动，最大不能超过目标坐标，反之向左移动最小不能小于目标坐标
-    move() {
-      var movepos = this.moveFun(this.opos, this.tpos, this.x);
-   
-      this.x = (this.opos.x > this.tpos.x) ? Math.max(this.x - movepos.x, this.tpos.x) : Math.min(this.x + movepos.x, this.tpos.x)
-      this.y = this.opos.y > this.tpos.y ? Math.max(this.y - movepos.y, this.tpos.y) : Math.min(this.y + movepos.y, this.tpos.y)
-      this.r = Math.max(Math.abs((this.r - Math.random() / 1.2).toFixed(2)), 0)
-   
     }
   }
-   
-  class BigCircle {
-    constructor(x, y) {
-      this.bR = random(16, 32)
-      this.overR = random(60, 100)
-      this.x = x
-      this.y = y
-      this.op = 1
-    }
-   
-    creatBigCircle(ctx) {
-      ctx.beginPath()
-      ctx.arc(this.x, this.y, this.bR, 0, 2 * Math.PI)
-      ctx.closePath()
-      ctx.strokeStyle = 'rgba(128, 128, 128, ' + this.op + ')'
-      ctx.stroke()
-    }
-   
-    changeR() {
-      this.bR = Math.min(this.bR += random(1, 4), this.overR);
-      this.op = Math.max((this.op - Math.random() / 12).toFixed(2), 0)
-    }
-   
-    //检查是否运行完毕，以大圆为标准清空屏幕
-    complete() {
-      return this.bR >= this.overR && this.op <= 0;
-    }
-  }
-   
-  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-   
-  window.clearRequestTimeout = window.cancelAnimationFrame || window.mozCancelRequestAnimationFrame || window.webkitCancelRequestAnimationFrame || window.msCancelRequestAnimationFrame;
-   
-   
-  let c = document.getElementById("fireworks"),
-    w = c.width = c.offsetWidth,
-    h = c.height = c.offsetHeight,
-   
-    ctx = c.getContext("2d"),
-    nums = 40,
-    circles = [],
-    bCircle = null,
-    animationId = false;
-   
-  let int = function(x, y) {
-    circles = []
-   
-    if (animationId) clearRequestTimeout(animationId)
-   
-    for (let i = nums; i-- > 0;) {
-      circles.push(new Circle(x, y))
-    }
-   
-    bCircle = new BigCircle(x, y)
-    creat()
-  }
-   
-  let creat = function() {
-    ctx.clearRect(0, 0, w, h);
-   
-    circles.forEach(function(v) {
-      v.move();
-      v.creatCircle(ctx)
-    })
-   
-    bCircle.changeR()
-    bCircle.creatBigCircle(ctx)
-   
-    animationId = requestAnimationFrame(creat)
-   
-    if (bCircle.complete()) {
-      //以大圆为标准，清空屏幕停止动画
-      ctx.clearRect(0, 0, w, h);
-      clearRequestTimeout(animationId)
-    }
-  }
-   
-  c.onclick = function(e) {
-    e = e || window.event;
-    int(e.clientX, e.clientY)
-  }
+  clickEffect();//调用特效函数
+  
